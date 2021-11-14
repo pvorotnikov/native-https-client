650fb52f35adea7be1206984c8148a0b9de6862e
Petar Vorotnikov <petar@vorotnikov.net>
2021-11-14 07:23:06 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,25 @@
+# Native HTTPS client
+
+This is a tiny library intended to provide HTTPS client capabilities
+based on the native HTTPS library of NodeJS. It comes without any
+external dependencies so that it can be used in vanilla NodeJS environment.
+
+# Usage
+
+```javascript
+import { NativeHttpsClient } from 'native-https-client'
+
+(async function() {
+
+  // create client instance
+  const client = new NativeHttpsClient({
+    host: 'jsonplaceholder.typicode.com'
+  });
+
+  // get and log users
+  const users = await client.get('/users');
+  console.log(users);
+
+})();
+
+```
---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import https, { RequestOptions } from 'https';
+
+type NativeHttpsClientOptions = RequestOptions & {
+  throwOnErrors?: boolean
+}
+
+export class NativeHttpsClient {
+
+  private options: NativeHttpsClientOptions;
+
+  // create a client
+  constructor(options = {}) {
+    this.options = {
+      rejectUnauthorized: true,
+      throwOnErrors: true,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      ...options
+    }
+  }
+
+  // update the default options of the client
+  async setOptions(options: NativeHttpsClientOptions) { this.options = { ...this.options, ...options } }
+
+  // wrappers around doRequest
+  async get(path: string, options: NativeHttpsClientOptions = {}) { return this.doRequest({ ...options, path, method: 'GET' }) }
+  async post(path: string, data: any, options: NativeHttpsClientOptions = {}) { return this.doRequest({ ...options, path, method: 'POST' }, data) }
+  async put(path: string, data: any, options: NativeHttpsClientOptions = {}) { return this.doRequest({ ...options, path, method: 'PUT' }, data) }
+  async delete(path: string, data: any, options: NativeHttpsClientOptions = {}) { return this.doRequest({ ...options, path, method: 'DELETE' }, data) }
+
+  // do the actual request
+  async doRequest(options: NativeHttpsClientOptions, data: any = null) {
+
+    return new Promise(resolve => {
+
+      const configOptions = { ...this.options, ...options };
+
+      const req = https.request(configOptions, res => {
+
+        console.log(`code=${res.statusCode}`);
+
+        // construct response
+        let response = '';
+        res.on('data', chunk => response += chunk);
+
+        // return response
+        res.on('close', () => {
+
+          if (res.statusCode || 1000 >= 400 && configOptions.throwOnErrors) {
+            throw new Error(`code=${res.statusCode}, body=${response}`);
+          }
+
+          try {
+            response = JSON.parse(response);
+          } catch (err) { };
+
+          resolve(response);
+        });
+
+        res.on('error', err => { // handle errors on the response
+          console.error(err.message);
+          throw err
+        });
+
+      });
+
+      req.on('error', err => { // handle errors on the request
+        console.error(err.message);
+        throw err;
+      });
+
+      if (data) {
+        const body = typeof data === 'string' ? data : JSON.stringify(data);
+        req.write(body);
+      }
+
+      req.end();
+
+    });
+
+  }
+
+}
